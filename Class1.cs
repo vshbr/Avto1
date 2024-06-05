18bcabc5ffe463edaeacd184d671b05c4d8d0176
Ivan <Ivan@DESKTOP-KOA2PSB>
2024-06-05 03:01:43 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Avto
+{
+    private string nomAvto;
+    private int benzBak;
+    private float rashod;
+
+    public Avto()
+    {
+        nomAvto = " ";
+        benzBak = 0;
+        rashod = 0.0f;
+    }
+
+    public Avto(string nom, int bak, float ras)
+    {
+        nomAvto = nom;
+        benzBak = bak;
+        rashod = ras;
+    }
+
+    public void info(string nom, float bak, float ras)
+    {
+        nomAvto = nom;
+        benzBak = (int)bak;
+        rashod = ras;
+    }
+
+    public void outInfo()
+    {
+        Console.WriteLine($"Номер авто: {nomAvto}, Количество бензина в баке: {benzBak}, Расход топлива на 100 км: {rashod}");
+    }
+
+    public void zapravka(float top)
+    {
+        benzBak += (int)top;
+    }
+
+    public void move(int km)
+    {
+        int fuelNeeded = (int)(km * rashod / 100);
+        if (fuelNeeded <= benzBak)
+        {
+            benzBak -= fuelNeeded;
+            Console.WriteLine($"Проехали {km} км.");
+        }
+        else
+        {
+            Console.WriteLine("Недостаточно топлива для поездки.");
+        }
+    }
+
+    public int ostatok()
+    {
+        return benzBak;
+    }
+
+    public void tormozhenie()
+    {
+        Console.WriteLine("Выполнено торможение.");
+    }
+
+    public void razgon()
+    {
+        Console.WriteLine("Машина разогнана.");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите номер авто:");
+        string nom = Console.ReadLine();
+
+        Console.WriteLine("Введите объем бензина в баке:");
+        float bak = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите расход топлива на 100 км:");
+        float ras = float.Parse(Console.ReadLine());
+
+        Avto myCar = new Avto(nom, (int)bak, ras);
+
+        while (true)
+        {
+            Console.WriteLine("Выберите действие:\n1 - Езда\n2 - Заправка\n3 - Торможение\n4 - Разгон\n5 - Выйти");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Введите расстояние:");
+                    int distance = int.Parse(Console.ReadLine());
+                    myCar.move(distance);
+                    break;
+                case 2:
+                    Console.WriteLine("Введите количество залитого бензина:");
+                    float topUp = float.Parse(Console.ReadLine());
+                    myCar.zapravka(topUp);
+                    break;
+                case 3:
+                    myCar.tormozhenie();
+                    break;
+                case 4:
+                    myCar.razgon();
+                    break;
+                case 5:
+                    return;
+                default:
+                    Console.WriteLine("Некорректный выбор. Попробуйте еще раз.");
+                    break;
+            }
+            Console.WriteLine($"Остаток топлива в баке: {myCar.ostatok()} л.");
+        }
+    }
+}
